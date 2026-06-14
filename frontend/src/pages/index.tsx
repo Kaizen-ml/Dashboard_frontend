fb2d76b740bf4649f4da78cb9011fc4648e7ed62
Vrinda-28 <vrindagarg2811@gmail.com>
2026-06-14 14:54:38 +0530

    Completely remove user profile component from navbar

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -247,13 +247,7 @@ export default function App() {
               </svg>
               <span className="notification-badge"></span>
             </button>
-            <div className="user-profile">
-              <div className="avatar">
-                <svg width="16" height="16" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
-                </svg>
-              </div>
-            </div>
+
           </div>
         </header>
 
